66f6bd19e682f91ec93b239a29286c1dc36f85df
SantiagoGaray32 <santi.garay2811@gmail.com>
2022-08-18 18:17:17 -0300

    agregando archivos app react

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../style/footer.css"
+
+export default function Footer() {
+    return (
+        <div className="footer-container"> 
+            <p>MyTinerary © 2022 All rights Reserved</p>
+            
+        </div>
+    )
+}
