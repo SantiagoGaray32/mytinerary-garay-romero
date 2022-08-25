4d3ced83f62121d1918264d128644c20d8d5140c
FranRomero209 <francromero@protonmail.com>
2022-08-24 21:00:19 -0300

    navbar js+css y layout actualizado

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../style/navbar.css";
+import User from "../img/user.png";
+import logoMenu from "../img/menu.png";
+
+function Navbar() {
+  return (
+    <div className="navContainer">
+      <input type="checkbox" id="btn-menu"></input>
+      <label for="btn-menu">
+        <img className="logoMenu" src={logoMenu}></img>
+      </label>
+      <div className="menu">
+        <ul>
+          <li>
+            <a href="">Cities</a>
+          </li>
+          <li>
+            <a href="">New City</a>
+          </li>
+          <li>
+            <a href="">Under Construction</a>
+          </li>
+          <li>
+            <a>
+              <img className="logoUser" src={User}></img>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
