e1daceb6e702d0ffbc65cdab5ee68ce9e9e9258f
SantiagoGaray32 <santi.garay2811@gmail.com>
2022-09-15 22:01:50 -0300

    agregando componentes y paginas

---
+++ src/pages/NewItinerary.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NewItinerary from "../components/NewItinerary";
+import UnderConstruction from "./UnderConstruction";
+
+function NewItineraryPage() {
+  return (
+    <>
+      <NewItinerary />
+      <UnderConstruction />
+    </>
+  );
+}
+
+export default NewItineraryPage;
