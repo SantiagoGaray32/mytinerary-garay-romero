154113cfe23140d07eb6c71d23f68d87d773f43d
SantiagoGaray32 <santi.garay2811@gmail.com>
2022-09-24 09:44:07 -0300

    configrando browser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter} from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -9,7 +10,9 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
