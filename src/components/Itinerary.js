9fe709927b4e1d29f8b190af9ca1531d5fba44d4
SantiagoGaray32 <santi.garay2811@gmail.com>
2022-09-24 15:32:58 -0300

    correcciones

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -1,16 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setItineraries } from "../features/itinerariesSlices";
-import { useGetItinerariesByCityIdQuery } from "../features/citiesAPI";
+import { useGetItinerariesByCityIdQuery, useLikeDislikeMutation } from "../features/citiesAPI";
 import Activities from "./Activities";
 import Comments from "./Comments";
 import "../styles/Itinerary.css";
+import likeImg from "../img/like.png"
+import dislikeImg from "../img/dislike.png"
+import PostComment from "./PostComment";
 
 function Itinerary(props) {
   const cityId = props.cityId;
   const dispatch = useDispatch();
   const itineraries = useSelector((state) => state.itineraries.itineraries);
+  const user = useSelector(state => state.user.user);
   const { data } = useGetItinerariesByCityIdQuery(cityId);
+  const [likeDislike] = useLikeDislikeMutation();
+  const [reload,setReload] = useState(true);
+  const [image, setImage] = useState('');
+  //const [data, setData] = useState ({})
+  const [liked, setLiked] = useState(false)
+//  const training = setLiked(!liked);
 
   useEffect(() => {
     if (data && data.success) {
@@ -18,8 +28,19 @@ function Itinerary(props) {
     }
   }, [data]);
 
+  const likeItinerary = async (id) => {
+    let res = await likeDislike({itineraryId: id, userId: user?.id});
+    if(res.data?.success){
+      setReload(!reload)
+    } else {
+      console.log(res.error);
+    }
+  }
+
+ 
+
   const renderItinerary = (it) => {
-    return (
+    return ( data &&
       <div className="itineraryContainer" key={it._id}>
         <div>
           <h4>Itinerary:<p>{it.itinerary}</p></h4>
@@ -34,6 +55,13 @@ function Itinerary(props) {
             {it.tags.map((tag) => (
               <p key={tag}>{tag}</p>
             ))}
+          </div>
+          <div className="itineraryLikes">
+            <button onClick={() => likeItinerary(it._id, setLiked(!liked))}   > 
+            {/* <img src={ liked && it.likes.includes(user.id) ? `${dislikeImg}` : `${likeImg}` } className='likeButton' alt='like' /> */}
+            <img src={liked ?  `${likeImg}` : `${dislikeImg}`} className='likeButton' alt='like' />
+          </button>
+            <p>{it.likes[0]}</p>
           </div>
         </div>
         <div className="activitiesContainer">
@@ -51,5 +79,4 @@ function Itinerary(props) {
     </div>
   );
 }
-
 export default Itinerary;
